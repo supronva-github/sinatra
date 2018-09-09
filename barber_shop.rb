31ddf6e1f6a4c4312ca9388ae8cc3dcb7e8aac23
Supronva <supronva@gmail.com>
2018-09-10 00:09:09 +0300

    create database for barbershop

--- barber_shop.rb
+++ barber_shop.rb
@@ -1,5 +1,18 @@
 !#/usr/bin/ruby
 require 'sinatra'
+require 'sqlite3'
+
+configure do
+  @db = SQLite3::Database.new 'barber_shop.db'
+  @db.execute 'CREATE TABLE "users"
+  (
+    "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+    "username" TEXT,
+    "phone" TEXT,
+    "date" TEXT, 
+    "barber" TEXT
+  );'
+end
 
 get '/' do
   erb :index
@@ -9,8 +22,9 @@ post '/' do
   @user_name = params[:username]
   @phone = params[:phone]
   @date = params[:date]
+  @barber = params[:barber]
   File.open('./public/client.txt','a') do |file|
-    file.puts "Client: #{@user_name};Phone: #{@phone}; Date and time: #{@date}"
+    file.puts "Client: #{@user_name};Phone: #{@phone}; Date and time: #{@date}, barber: #{@barber}"
   end
   erb :messages
 end
